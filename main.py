1e71d4444ef0a4126f026bf73faa52de4c16617b
lisanne <lisannevannes@gmail.com>
2021-03-04 20:34:07 +0100

    Sessie 4 maart nog bezig met 4.4 - model moet nog worden verbeterd

--- main.py
+++ main.py
@@ -8,63 +8,89 @@ import gym
 import random
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 from fleur_lisanne_hannah.funcs import *
 from fleur_lisanne_hannah.models import get_model
 
 env = gym.make("CartPole-v0")
-env.reset()
-done = False
-observation_all, reward_all, is_done_all, info_all, action_all = [], [], [], [], []
-observation_t, reward_t, is_done_t, info_t, action_t = [], [], [], [], []
 
-agent = get_model(3e-5)
+optimizer = tf.keras.optimizers.Adam(3e-5)
+aantal_stappen = []
+batch_size = 20
+epochs = 100
+for __ in range(10):
 
-for i in range(100):
-    observation_i = env.reset()
-    reward_i = 1
-    info_i = {}
-
-    while not done:
-        observation_t.append(observation_i)
-        reward_t.append(reward_i)
-        is_done_t.append(done)
-        info_t.append(info_i)
-
-        predictions = agent.predict(observation_i.reshape((1,-1)))[0][0]
-        if predictions <= random.random():
-            action = 0
-        else:
-            action = 1
-        observation_i, reward_i, done, info_i = env.step(action)
-        action_t.append(action)
-
-
-
-        env.render()
-        if done:
-            observation_all.append(observation_t)
-            reward_all.append(reward_t)
-            is_done_all.append(is_done_t)
-            info_all.append(info_t)
-            action_all.append(action_t)
-            observation_t, reward_t, is_done_t, info_t, action_t = [], [], [], [], []
+    env.reset()
     done = False
+    observation_all, reward_all, is_done_all, info_all, action_all = [], [], [], [], []
+    observation_t, reward_t, is_done_t, info_t, action_t = [], [], [], [], []
 
-# reward_memory = decay_and_normalize(reward_all, 0.9)
 
-observation_memory = np.concatenate(observation_all)
-action_memory = np.concatenate(action_all)
-reward_memory = np.concatenate(reward_all)
-is_done_memory = np.concatenate(is_done_all)
 
-optimizer = tf.keras.optimizers.Adam(3e-4)
-agent.compile(optimizer, tf.keras.losses.mse)
-agent.fit(observation_memory, action_memory)
+    agent = get_model()
 
-# optimizer = tf.keras.optimizers.Adam(3e-4)
-# with tf.GradientTape() as tape:
-#     predictions = agent(observation_memory)
-#     loss = tf.keras.losses.mse(action_memory, predictions)
-# train_vars = agent.trainable_variablesgrads = tape.gradient(loss, train_vars)
-# optimizer.apply_gradients(zip(grads, train_vars))
+    for i in range(epochs):
+        observation_i = env.reset()
+        reward_i = 1
+        info_i = {}
+
+        while not done:
+            observation_t.append(observation_i)
+            reward_t.append(reward_i)
+            is_done_t.append(done)
+            info_t.append(info_i)
+
+            predictions = agent.predict(observation_i.reshape((1,-1)))[0][0]
+            if predictions <= random.random():
+                action = 0
+            else:
+                action = 1
+            observation_i, reward_i, done, info_i = env.step(action)
+            action_t.append(action)
+
+
+
+            #env.render()
+            if done:
+                print("aantal stappen: ", len(observation_t))
+                aantal_stappen.append(len(observation_t))
+                observation_all.append(observation_t)
+                reward_all.append(reward_t)
+                is_done_all.append(is_done_t)
+                info_all.append(info_t)
+                action_all.append(action_t)
+                observation_t, reward_t, is_done_t, info_t, action_t = [], [], [], [], []
+        done = False
+
+
+    observation_memory = np.concatenate(observation_all)
+    action_memory = np.concatenate(action_all)
+    is_done_memory = np.concatenate(is_done_all)
+
+    # niet plat slaan, want je wilt weten welke potje en je moet normaliseren)
+    decayed_reward = decay_and_normalize(reward_all, 0.97)
+
+
+    # optimizer = tf.keras.optimizers.Adam(3e-4)
+    # agent.compile(optimizer, tf.keras.losses.mse)
+    # agent.fit(observation_memory, action_memory)
+
+    obs_split = np.array_split(observation_memory, int(len(action_memory)/batch_size))
+    action_split = np.array_split(action_memory, int(len(action_memory)/batch_size))
+    decay_split = np.array_split(decayed_reward, int(len(action_memory)/batch_size))
+
+    for s in range(len(obs_split)):
+
+        with tf.GradientTape() as tape:
+            predictions = agent(obs_split[s])
+            loss = tf.keras.losses.mse(action_split[s], predictions)
+            loss = loss * decay_split[s]
+
+        train_vars = agent.trainable_variables
+        grads = tape.gradient(loss, train_vars)
+        optimizer.apply_gradients(zip(grads, train_vars))
+
+#print(aantal_stappen)
+plt.plot(np.arange(1, len(aantal_stappen)+1), aantal_stappen)
+plt.show()
--- models.py
+++ models.py
@@ -1,9 +1,14 @@
 import tensorflow as tf
 
-def get_model(lr):
+def get_model():
     input1 = tf.keras.layers.Input(4)
-    hidden1 = tf.keras.layers.Dense(128, "relu")(input1)
-    output = tf.keras.layers.Dense(1, "sigmoid")(hidden1)
+    X = tf.keras.layers.Dense(128, "relu")(input1)
+    X = tf.keras.layers.Dense(256, "relu")(X)
+    X = tf.keras.layers.Dropout(.2)(X)
+    X = tf.keras.layers.Dense(256, "relu")(X)
+    X = tf.keras.layers.Dropout(.2)(X)
+    X = tf.keras.layers.Dense(128, "relu")(X)
+    output = tf.keras.layers.Dense(1, "sigmoid")(X)
     model = tf.keras.models.Model(inputs=[input1], outputs=[output])
 
 
